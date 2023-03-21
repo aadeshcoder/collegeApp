acf9dd3a9d3d11743230094ffc6f1853970a6547
aadeshcoder <aadeshtendolkar@gmail.com>
2023-03-21 14:44:12 +0530

    completed initial folder structure

---
+++ src/app/app-view/app-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-view',
+  templateUrl: './app-view.component.html',
+  styleUrls: ['./app-view.component.scss']
+})
+export class AppViewComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppViewComponent } from './app-view/app-view.component';
+import { SharedModule } from './shared/shared.module';
+import { TopNavComponent } from './app-view/components/top-nav/top-nav.component';
+import { LeftNavComponent } from './app-view/components/left-nav/left-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppViewComponent,
+    TopNavComponent,
+    LeftNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]

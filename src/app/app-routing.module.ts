deb6f645290d5da3db2d43b2e6d27acad1b88dc6
aadeshcoder <aadeshtendolkar@gmail.com>
2023-03-23 09:48:27 +0530

    chaged some styling and added lazy loading to the Student module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { AppViewComponent } from './app-view/app-view.component';
 
 const routes: Routes = [
   {
-    path:"",
-    redirectTo:"app-view",
-    pathMatch:'full'
+    path: "",
+    redirectTo: "app-view",
+    pathMatch: 'full'
   },
   {
-    path:"app-view",
-    component:AppViewComponent
+    path: "app-view",
+    component: AppViewComponent,
+    children: [
+      {
+        path: "student",
+        loadChildren: () => import('./feature-modules/student/student.module').then(m => m.StudentModule)
+      }
+    ]
   }
 ];
 
--- src/app/feature-modules/student/student.module.ts
+++ src/app/feature-modules/student/student.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentListComponent } from './components/student-list/student-list.component';
+import { Routes,RouterModule } from '@angular/router';
 
-
+// Routing for student module
+const routes:Routes = [
+  {
+    path:'',
+    component:StudentListComponent
+  }
+]
 
 @NgModule({
   declarations: [
     StudentListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
-export class StudentModule { }
+export class StudentModule { 
+
+  constructor() {
+    console.log("Student Module")
+  }
+}
